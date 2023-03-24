90fee07167f27af8bc0714d47290b07e0967d92e
brahimisgreat <120285983+brahimisgreat@users.noreply.github.com>
2023-03-24 05:26:59 -0400

    Add files via upload

---
+++ FULL STACK CRAP/JAVASCRIPT/practice.js
@@ -0,0 +1,6 @@
+function randomnumber (){
+    let number = Math.ceil(Math.random() * 6)
+    return number
+}
+
+console.log(randomnumber())
---
+++ FULL STACK CRAP/JAVASCRIPT/securethevault.js
@@ -0,0 +1,11 @@
+// Created a alert dialog
+alert("You have received this message because you have been chosen\
+ to open an important vault.Here is the secret combination:");
+//created vault combination 1 
+let comb1 = 5 + 5
+//created vault combination 2
+let comb2 = 4 * 10 
+//created vault combination 3 
+let comb3 = 30 + 9
+//alert all combinations # 
+alert(`the combination is ${comb1}-${comb2}-${comb3}`)
